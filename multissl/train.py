1c02129e7b4341a1b86b6c62655d4d137730b77b
jurriandoornbos <59558756+jurriandoornbos@users.noreply.github.com>
2025-01-30 21:38:44 +0100

    oops

--- multissl/train.py
+++ multissl/train.py
@@ -79,7 +79,6 @@ def main():
 
     model = build_model(args)
     # Step 6: Checkpointer:
-    model(weights=None)
 
     checkpoint_callback = pl.callbacks.ModelCheckpoint(
         dirpath="checkpoints/",
