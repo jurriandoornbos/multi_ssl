39ac7dcad2d8c24351ae1babb235b9b2b7eab5fa
jurriandoornbos <59558756+jurriandoornbos@users.noreply.github.com>
2025-01-30 14:32:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Multispectral SSL",
+    name="multissl",
     version="0.1.0",
     author="Jurrian Doornbos",
     author_email="jurrian.dooornbos@wur.nl",
